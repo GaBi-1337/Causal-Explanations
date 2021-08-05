2b459cc627988e43b71cab55e2185bf020e2b806
G-a-B-i <70230822+G-a-B-i@users.noreply.github.com>
2021-08-05 17:36:29 -0400

    fixed major bugs
    ^

--- explain.py
+++ explain.py
@@ -5,15 +5,14 @@ from sklearn.ensemble import RandomForestClassifier
 from data import getData
 
 class explain(object):
-    def __init__(self, model, poi, data):
+    def __init__(self, model, poi):
         self.model = model
         self.poi = poi
-        self.data = data
-        self.N = set(np.arange(poi.shape[0]))
-        self.fra = None
+        self.N = set(np.arange(poi.shape[1]))
+        self.fra = np.array([])
         
-    def feasible_recourse_actions(self, k):
-        sorted_closest_points = np.array(sorted([(np.linalg.norm((self.data[i] - self.poi)), self.data[i]) for i in range(self.data.shape[0])], key = lambda row: row[0]))[:, 1]
+    def feasible_recourse_actions(self, data, k):
+        sorted_closest_points = np.array(sorted([(np.linalg.norm((data[i] - self.poi)), data[i]) for i in range(data.shape[0])], key = lambda row: row[0]))[:, 1]
         k_nearest_points = list()
         k_counter = 0
         for point in sorted_closest_points:
@@ -23,9 +22,10 @@ class explain(object):
                 if k_counter == k:
                     break
         self.fra = np.array(k_nearest_points)
+        return self
 
     def value(self, S):
-        if self.fra == None:
+        if len(self.fra) == 0:
             raise ValueError("There are no feasible recourse actions")
         for point in self.fra:
             xp = list()
@@ -43,35 +43,37 @@ class explain(object):
         for i in range(len(S)):
             if S[i] == 1:
                 temp = S.copy()
-                temp[i] = 0 if temp[i] == 1 else 1
+                temp[i] = 0
                 if self.value(S) != self.value(temp):
-                    print("hi")
                     χ.add(i)
         return χ
     
     def _minimal_causes(self, S, i, quasi = False):
-        temp = S.copy()
-        temp[i] = 0 if temp[i] == 1 else 1
-        if self.value(S) == self.value(temp):
-            return False
-        for j in range(len(S)):
-            if S[j] == 1 and j != i:
-                temp[j] = 0 if temp[j] == 1 else 1
-                if self.value(S) != self.value(temp):
-                    if quasi:
-                        return True
-                    else:
-                        continue
-                else:
-                    if quasi:
-                        continue
-                    else:
-                        return False
-        return True
+        if S[i] != 0:
+            temp = S.copy()
+            temp[i] = 0
+            if self.value(S) != self.value(temp):
+                if not quasi:
+                    temp[i] = 1
+                    for j in range(len(S)):
+                        if S[j] != 0 and j != i:
+                            temp[j] = 0
+                            if self.value(S) != self.value(temp):
+                                if quasi:
+                                    return True
+                                else:
+                                    temp[j] = 1
+                            else:
+                                if quasi:
+                                    temp[j] = 1
+                                else:
+                                    return False
+                return True
+        return False
 
     def sample(self, m, index, seed=0):
         indices = { "Johnston": lambda S, i: (1 / len(self._critical_features(S))) if self._minimal_causes(S, i, quasi = True) else 0, 
-                    "Deegan-Packel": lambda S, i: (1 / S.count(1)) if self._minimal_causes(S, i) else 0, 
+                    "Deegan-Packel": lambda S, i: (1 / np.sum(S)) if self._minimal_causes(S, i) else 0, 
                     "Holler-Packel": lambda S, i: 1 if self._minimal_causes(S, i) else 0}
         if index not in indices:
             raise ValueError("Can't compute " + index + " index")
@@ -79,8 +81,7 @@ class explain(object):
         random.seed(seed)            
         for _ in range(m):
             S = [random.randint(0, 1) for _ in range(len(self.N))]
-            for i in self.N:
-                unbiased_estimate[i] += 2 * indices[index](S, i)
+            unbiased_estimate = np.array(list(map(lambda i: unbiased_estimate[i] + (2 * indices[index](S, i)) , np.arange(len(self.N)))))
         return unbiased_estimate / m
 
     def responsibility_index(self, m, seed=0):
@@ -90,7 +91,7 @@ class explain(object):
             S = [random.randint(0, 1) for _ in range(len(self.N))]
             for i in self.N:
                 if self._minimal_causes(S, i, quasi = True):
-                    unbiased_estimate[i] = max(unbiased_estimate[i], 1/S.count(1))
+                    unbiased_estimate[i] = max(unbiased_estimate[i], 1 / np.sum(S))
         return unbiased_estimate
         
 def main():
@@ -98,17 +99,7 @@ def main():
     model = RandomForestClassifier(n_estimators=50, max_features=None, n_jobs=-1, random_state=0).fit(X_trn, Y_trn)
     poi = X_tst[0].toarray().reshape(1,-1)
     data = X_tst[1: 1000]
-    print(poi)  
-    exp = explain(model, poi, data)
-    print(model.predict(poi))
-    S = [random.randint(0, 1) for _ in range(poi.shape[1])]
-    print(exp._minimal_causes(S, 1, True))
-    """for i in range(poi.shape[1]):
-        print(i)
-        if exp._minimal_causes(S, i, True):
-            print(len(exp._critical_features(S)))
-            #print(S.count(0))"""
-    
+    exp = explain(model, poi).feasible_recourse_actions(data, 10)
 
 
 if __name__ == "__main__":
